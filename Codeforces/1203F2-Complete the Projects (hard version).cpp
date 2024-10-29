d80f8c02bde6eac743da690e112e2b2e4e5ee4e7
PATO <ahmed9pato7@gmail.com>
2024-10-30 01:47:24 +0300

    DONE

--- Codeforces/1203F2-Complete the Projects (hard version).cpp
+++ Codeforces/1203F2-Complete the Projects (hard version).cpp
@@ -8,24 +8,18 @@ using namespace std;
 void Ahmed_Sayed(){
 ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 }
+int n, r , dp[100][(ll)1e5];
+vector<pair<int, int>>pos, neg;
+int rec(int i = 0,int rem = r){
+    if(i == neg.size())return 0;
 
-bool cmp(pair<int,int>p,pair<int,int>p2){
-    if(p.first+p.second!=p2.first+p2.second)
-        return p.first+p.second>p2.first+p2.second;
-}
-
-int n,r,dp[100][(ll)1e5];
-vector<pair<int,int>>pos,neg;
-int rec(int i=0,int rem=r){
-    if(i==neg.size())return 0;
-
-    int&ret=dp[i][rem];
+    int &ret = dp[i][rem];
     if(~ret)return ret;
 
-    ret=rec(i+1,rem);
+    ret=rec(i + 1, rem);
 
-    if(rem>=neg[i].first&&rem+neg[i].second>=0)
-        ret=max(ret,1+rec(i+1,rem+neg[i].second));
+    if(rem >= neg[i].first && rem + neg[i].second >= 0)
+        ret=max(ret ,1 + rec(i + 1, rem + neg[i].second));
 
     return ret;
 }
@@ -33,23 +27,23 @@ int rec(int i=0,int rem=r){
 int main()
 {
 Ahmed_Sayed();
-cin>>n>>r;
+cin >>n >> r;
 memset(dp,-1,sizeof dp);
 
-for(int i=0;i<n;i++){
-    int a,b;cin>>a>>b;
+for(int i = 0;i < n; i++){
+    int a,b;cin >> a >> b;
 
-    if(b<0)neg.push_back({a,b});
-    else pos.push_back({a,b});
+    if(b < 0)neg.push_back({a, b});
+    else pos.push_back({a, b});
 }
-sort(all(pos)),sort(all(neg),cmp);
+sort(all(pos)),sort(all(neg));
 
-int ans=0;
+int ans = 0;
 for(auto [a,b]:pos){
-    if(a>r)break;
+    if(a > r)break;
 
-    r+=b,ans++;
+    r += b,ans++;
 }
 
-cout<<ans+rec();
+cout<<ans + rec();
 }
