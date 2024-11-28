843e44bef4f05ca9403ab81a29037d8c6e1700dd
PATO <ahmed9pato7@gmail.com>
2024-11-28 18:38:42 +0200

    DONE

--- LeetCode/3362. Zero Array Transformation III.cpp
+++ LeetCode/3362. Zero Array Transformation III.cpp
@@ -1,98 +1,34 @@
-#define ll long long
 class Solution {
 public:
-    int v[(ll)1e5+5];
-
-    struct st{
-        int mx,lazy;
-    };
-    
-    st seg[(ll)5e5];
-
-    void push(int node,int l,int r){
-        seg[node].mx+=seg[node].lazy;
-
-        if(l!=r){
-            seg[node*2].lazy+=seg[node].lazy,
-            seg[node*2+1].lazy+=seg[node].lazy;
-        }
-        seg[node].lazy=0;
-    }
-
-    void bul(int node,int l,int r){
-        if(l==r){
-            seg[node].mx=v[l];
-            return;
-        }
-
-        int m=(l+r)>>1;
-        bul(node*2,l,m),
-        bul(node*2+1,m+1,r);
-
-        seg[node].mx=max(seg[node*2].mx,seg[node*2+1].mx);
-    }
-
-      int get(int node,int l,int r,int lef,int righ){
-        push(node,l,r);
-          
-        if(l>righ||r<lef)return -1e9;
-        if(l>=lef&&r<=righ)return seg[node].mx;
-
-        int m=(l+r)>>1;
-        return max(get(node*2,l,m,lef,righ),get(node*2+1,m+1,r,lef,righ));
-      }
-
-     void up(int node,int l,int r,int lef,int righ){
-        push(node,l,r);
-        
-        if(l>righ||r<lef)return;
-        if(l>=lef&&r<=righ){
-            seg[node].lazy++,
-            push(node,l,r);
-            
-            return;
-        }
-
-        int m=(l+r)>>1;
-        up(node*2,l,m,lef,righ),
-        up(node*2+1,m+1,r,lef,righ);
-
-        seg[node].mx=max(seg[node*2].mx,seg[node*2+1].mx);
-      }
-
 
     int maxRemoval(vector<int>& x, vector<vector<int>>& q) {
-        x.insert(x.begin()+0,0);
+        int n=x.size();
 
-        int n=x.size()-1;
-        vector<ll>pre(n+5);
+        sort(q.begin(),q.end());
+       
+        vector<vector<int>>adj(n);
+        for(auto i:q)adj[i[0]].push_back(i[1]);
 
-        for(auto i:q)
-            pre[i[0]+1]--,pre[i[1]+2]++;
-        
-        for(int i=1;i<=n;i++){
-            pre[i]+=pre[i-1],
-            x[i]+=pre[i];
-            
-            if(x[i]>0)return -1;
-        }
+        priority_queue<int>pq;
 
-        for(int i=0;i<x.size();i++)v[i]=x[i];
-        bul(1,1,n);
-    
-        vector<array<ll,3>>qq;
-        for(auto i:q){
-            qq.push_back({abs(i[0]-i[1]),i[0],i[1]});
-        }
+        int c=0;
+        vector<int>pre(n+1);
+        for(int i=0;i<n;i++){
+            for(auto j:adj[i])pq.push(j);
 
-        sort(qq.begin(),qq.end());
-        int ans=0;
-        for(auto i:qq)
-            if(get(1,1,n,i[1]+1,i[2]+1)<0){
-                up(1,1,n,i[1]+1,i[2]+1);
-                ans++;
+            c+=pre[i],
+            x[i]-=c;
+
+            while(x[i]>0){
+                if(!pq.size()||pq.top()<i)return -1;
+
+                c++,
+                pre[pq.top()+1]--,
+                pq.pop();
+                x[i]--;
             }
-
-        return ans;
+        }
+       
+        return pq.size();
     }
 };
